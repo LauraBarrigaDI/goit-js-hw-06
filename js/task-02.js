ff3f11d63eb6cabbfee23b90c86c6649a6da278c
LauraBarrigaDI <laurabarriga31@gmail.com>
2023-03-13 20:39:58 -0500

    Task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,4 @@
+const ul = document.querySelector('ul#ingredients');
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +7,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const liArray = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+
+  li.textContent = "Potatoes";
+
+  li.classList.add('item');
+
+  return li;
+});
+
+ul.append(...ingredients);

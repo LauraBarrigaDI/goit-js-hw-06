26da7e89fba2614d1d16b7c9a4590a7353297c19
LauraBarrigaDI <laurabarriga31@gmail.com>
2023-03-13 20:49:19 -0500

    Task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // previene la recarga de la página al enviar el formulario
+  
+  const email = loginForm.elements.email.value;
+  const password = loginForm.elements.password.value;
+
+  if (!email || !password) {
+    alert('Por favor complete todos los campos');
+  } else {
+    const formData = {
+      email: email,
+      password: password
+    };
+    console.log(formData);
+    loginForm.reset(); // borra los valores de los campos del formulario
+  } 
+}); 
